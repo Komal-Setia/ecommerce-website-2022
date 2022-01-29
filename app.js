ca665221e534718bbf03c238fde560fc568dd24e
Komal <komal0527.cse19@chitkara.edu.in>
2022-01-29 12:50:43 +0530

    commit

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.use((req,res,next)=>{
     res.locals.success=req.flash('success');
     res.locals.error=req.flash('error');
     res.locals.currentUser=req.user;
-    res.locals.seller=req.user;
     next();
 });
 
